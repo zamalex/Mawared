5f207f4e62e29ea442a40bdfa8778b145abe7da1
zamalex <mustafaezzaldin@outlook.com>
2020-08-25 12:39:51 +0200

    home fragment added support fragment chat fragment cart fragment send order fragment

--- app/src/main/java/creativitysol/com/mawared/sendorder/SendOrdersFragment.java
+++ app/src/main/java/creativitysol/com/mawared/sendorder/SendOrdersFragment.java
@@ -1,13 +1,17 @@
 package creativitysol.com.mawared.sendorder;
 
+import android.Manifest;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.location.Location;
 import android.os.Bundle;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -18,27 +22,41 @@ import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 import creativitysol.com.mawared.R;
 
 
-public class SendOrdersFragment extends Fragment {
+public class SendOrdersFragment extends Fragment implements OnMapReadyCallback {
 
 
     View v;
 
     Dialog addCoponDialog,timeDialog;
 
-    RecyclerView address_rv;
+    RecyclerView address_rv,products_rv;
 
-    BottomSheetDialog address_dialog;
+    BottomSheetDialog address_dialog,map_dailog,payDialog,ordersDialog;
 
     AddressAdapter adapter;
+    SentOrdersAdapter ordersAdapter;
 
-    ConstraintLayout add_copon,add_time,add_address;
+    MapView mapView;
+    GoogleMap map;
+
+    ImageView show_map,dismiss_map;
+
+    ConstraintLayout add_copon,add_time,add_address,add_payment,show_products;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -50,22 +68,42 @@ public class SendOrdersFragment extends Fragment {
         timeDialog = new Dialog(getActivity());
 
         adapter = new AddressAdapter();
+        ordersAdapter = new SentOrdersAdapter();
 
         addCoponDialog.setContentView(R.layout.copon_dialog);
         timeDialog.setContentView(R.layout.time_dialog);
         address_dialog = new BottomSheetDialog(getActivity(),R.style.AppBottomSheetDialogTheme);
+        map_dailog = new BottomSheetDialog(getActivity(),R.style.AppBottomSheetDialogTheme);
+        payDialog = new BottomSheetDialog(getActivity(),R.style.AppBottomSheetDialogTheme);
+        ordersDialog = new BottomSheetDialog(getActivity(),R.style.AppBottomSheetDialogTheme);
+
+        payDialog.setContentView(R.layout.payment_dialog);
+        map_dailog.setContentView(R.layout.map_dialog);
+        map_dailog.setCancelable(false);
 
         address_dialog.setContentView(R.layout.address_dialog);
+        ordersDialog.setContentView(R.layout.products_dialog);
 
         address_rv = address_dialog.findViewById(R.id.address_rv);
 
+        products_rv = ordersDialog.findViewById(R.id.products_rv);
+
+        products_rv.setLayoutManager(new LinearLayoutManager(getActivity()));
+        products_rv.setAdapter(ordersAdapter);
+
+        show_map = address_dialog.findViewById(R.id.show_map);
         address_rv.setLayoutManager(new LinearLayoutManager(getActivity()));
         address_rv.setAdapter(adapter);
 
-        Window window1 = addCoponDialog.getWindow();
-        Window window2 = timeDialog.getWindow();
-        window1.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        window2.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        mapView = map_dailog.findViewById(R.id.mapview);
+        dismiss_map = map_dailog.findViewById(R.id.dismiss_map);
+        mapView.onCreate(savedInstanceState);
+
+
+        mapView.getMapAsync(this);
+
+
+        setAlertDialogs();
 
         addCoponDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         timeDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
@@ -73,7 +111,16 @@ public class SendOrdersFragment extends Fragment {
         add_copon = v.findViewById(R.id.add_copon);
         add_time = v.findViewById(R.id.add_time);
         add_address = v.findViewById(R.id.add_address);
+        add_payment = v.findViewById(R.id.add_payment);
+        show_products = v.findViewById(R.id.show_products);
 
+
+        show_products.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ordersDialog.show();
+            }
+        });
 
         add_copon.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -96,23 +143,102 @@ public class SendOrdersFragment extends Fragment {
             }
         });
 
+        add_payment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                payDialog.show();
+            }
+        });
+
+        show_map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                map_dailog.show();
+            }
+        });
+
+        dismiss_map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                map_dailog.dismiss();
+            }
+        });
+
+       setDialogsFullScreen();
+
+
+        return v;
+    }
+
+    private void setAlertDialogs() {
+        Window window1 = addCoponDialog.getWindow();
+        Window window2 = timeDialog.getWindow();
+        Window window3 = payDialog.getWindow();
+        window1.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        window2.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        window3.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+    }
+
+    private void setDialogsFullScreen() {
         address_dialog.setOnShowListener(new DialogInterface.OnShowListener() {
             @Override
             public void onShow(DialogInterface dialog) {
 
-                    BottomSheetDialog dialogc = (BottomSheetDialog) dialog;
-                    // When using AndroidX the resource can be found at com.google.android.material.R.id.design_bottom_sheet
-                    FrameLayout bottomSheet =  dialogc.findViewById(R.id.design_bottom_sheet);
+                BottomSheetDialog dialogc = (BottomSheetDialog) dialog;
+                // When using AndroidX the resource can be found at com.google.android.material.R.id.design_bottom_sheet
+                FrameLayout bottomSheet =  dialogc.findViewById(R.id.design_bottom_sheet);
 
-                    BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
-                    setupFullHeight(bottomSheet);
-                    bottomSheetBehavior.setPeekHeight(Resources.getSystem().getDisplayMetrics().heightPixels);
-                    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+                setupFullHeight(bottomSheet);
+                bottomSheetBehavior.setPeekHeight(Resources.getSystem().getDisplayMetrics().heightPixels);
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
             }
         });
 
+        ordersDialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialog) {
 
-        return v;
+                BottomSheetDialog dialogc = (BottomSheetDialog) dialog;
+                // When using AndroidX the resource can be found at com.google.android.material.R.id.design_bottom_sheet
+                FrameLayout bottomSheet =  dialogc.findViewById(R.id.design_bottom_sheet);
+
+                BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+                setupFullHeight(bottomSheet);
+                bottomSheetBehavior.setPeekHeight(Resources.getSystem().getDisplayMetrics().heightPixels);
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+            }
+        });
+
+        payDialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialog) {
+
+                BottomSheetDialog dialogc = (BottomSheetDialog) dialog;
+                // When using AndroidX the resource can be found at com.google.android.material.R.id.design_bottom_sheet
+                FrameLayout bottomSheet =  dialogc.findViewById(R.id.design_bottom_sheet);
+
+                BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+                setupFullHeight(bottomSheet);
+                bottomSheetBehavior.setPeekHeight(Resources.getSystem().getDisplayMetrics().heightPixels);
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+            }
+        });
+
+        map_dailog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialog) {
+
+                BottomSheetDialog dialogc = (BottomSheetDialog) dialog;
+                // When using AndroidX the resource can be found at com.google.android.material.R.id.design_bottom_sheet
+                FrameLayout bottomSheet =  dialogc.findViewById(R.id.design_bottom_sheet);
+
+                BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+                setupFullHeight(bottomSheet);
+                bottomSheetBehavior.setPeekHeight(Resources.getSystem().getDisplayMetrics().heightPixels);
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+            }
+        });
     }
 
     private void setupFullHeight(View bottomSheet) {
@@ -120,4 +246,44 @@ public class SendOrdersFragment extends Fragment {
         layoutParams.height = WindowManager.LayoutParams.MATCH_PARENT;
         bottomSheet.setLayoutParams(layoutParams);
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        map.getUiSettings().setMyLocationButtonEnabled(true);
+//        map.setMyLocationEnabled(true);
+
+
+        // Updates the location and zoom of the MapView
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(43.1, -87.9), 15);
+        map.animateCamera(cameraUpdate);
+      //  map.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(43.1, -87.9)));
+
+    }
+
+    @Override
+    public void onResume() {
+        mapView.onResume();
+        super.onResume();
+    }
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
 }
---
+++ app/src/main/java/creativitysol/com/mawared/sendorder/SentOrdersAdapter.java
@@ -0,0 +1,60 @@
+package creativitysol.com.mawared.sendorder;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import creativitysol.com.mawared.R;
+import creativitysol.com.mawared.home.model.Product;
+
+
+
+public class SentOrdersAdapter extends RecyclerView.Adapter<SentOrdersAdapter.Holder> {
+
+
+    ArrayList<Product> products = new ArrayList<>();
+
+
+
+
+    @NonNull
+    @Override
+    public SentOrdersAdapter.Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_product_done, parent, false);
+        return new SentOrdersAdapter.Holder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final SentOrdersAdapter.Holder holder, final int position) {
+
+
+    }
+
+    public void setProducts(ArrayList<Product> products) {
+        this.products = products;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+
+
+        return 2;
+    }
+
+    class Holder extends RecyclerView.ViewHolder {
+
+        public Holder(@NonNull View itemView) {
+            super(itemView);
+
+        }
+    }
+
+
+}
